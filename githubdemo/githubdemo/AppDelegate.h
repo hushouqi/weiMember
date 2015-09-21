3ac522ac7b8f6117f3bef82ec8230f0f22974f24
hushouqi <hushouqi@hushouqideMacBook-Pro.local>
2015-09-21 15:12:36 +0800

    add

---
+++ githubdemo/githubdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubdemo
+//
+//  Created by hushouqi on 15/9/21.
+//  Copyright (c) 2015年 hushouqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubdemo/githubdemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  githubdemo
+//
+//  Created by hushouqi on 15/9/21.
+//  Copyright (c) 2015年 hushouqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
